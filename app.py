28deae2d2180c886eeb1735d2542f8e4f5dfb086
Gabriela Sanchez <74748682+gabrielasan20117@users.noreply.github.com>
2023-11-30 05:04:23 +0000

    logic for count number of round and win

--- app.py
+++ app.py
@@ -4,6 +4,10 @@
 #-----------------------------------------------------------------------------------------
 
 def play():
+    rounds = 0
+    player1_wins = 0
+    player2_wins = 0
+
     while True:
         player1 = input("Enter rock, paper, or scissors: ")
         player2 = input("Enter rock, paper, or scissors: ")
@@ -16,21 +20,32 @@ def play():
             print("It's a tie!")
         elif player1 == 'rock' and player2 == 'paper':
             print("Player 2 wins!")
+            player2_wins += 1
         elif player2 == 'rock' and player1 == 'paper':
             print("Player 1 wins!")
+            player1_wins += 1
         elif player1 == 'paper' and player2 == 'scissors':
             print("Player 2 wins!")
+            player2_wins += 1
         elif player2 == 'paper' and player1 == 'scissors':
             print("Player 1 wins!")
+            player1_wins += 1
         elif player1 == 'rock' and player2 == 'scissors':
             print("Player 1 wins!")
+            player1_wins += 1
         elif player2 == 'rock' and player1 == 'scissors':
             print("Player 2 wins!")
+            player2_wins += 1
         else:
             print("Invalid input. Please enter rock, paper, or scissors.")
         
+        rounds += 1
         play_again = input("Do you want to play again? (yes/no): ")
         if play_again.lower() != "yes":
             break
+    
+    print("Total rounds played:", rounds)
+    print("Player 1 wins:", player1_wins)
+    print("Player 2 wins:", player2_wins)
 
 play()
